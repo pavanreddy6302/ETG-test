beec9373f0d3f6056a102863cca4d6e8ccd3a5f0
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-02 12:31:48 +0530

    eks1

--- terraform/iam-test.tf
+++ terraform/iam-test.tf
@@ -97,7 +97,7 @@ resource "aws_iam_role" "github_actions_role" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo::*"
+          "token.actions.githubusercontent.com:sub" = "repo:pavanreddy6302/ETG-test:*"
         }
       }
     }]
