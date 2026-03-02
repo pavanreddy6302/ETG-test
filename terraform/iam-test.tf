21ad7082e33e8603b792a9207be421da35dd7a3f
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-02 15:43:24 +0530

    eks1

--- terraform/iam-test.tf
+++ terraform/iam-test.tf
@@ -288,3 +288,16 @@ resource "aws_eks_access_policy_association" "github_admin_policy" {
 
   depends_on = [aws_eks_access_entry.github_admin]
 }
+
+
+resource "aws_eks_access_policy_association" "github_cluster_admin_policy" {
+  cluster_name  = var.cluster_name
+  principal_arn = aws_iam_role.github_actions_role.arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.github_admin]
+}
