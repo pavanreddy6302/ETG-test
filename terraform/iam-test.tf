bf13eeb13a611b2f5658dc89c0e5a4a6ab683b10
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-02 11:24:19 +0530

    eks1

--- terraform/iam-test.tf
+++ terraform/iam-test.tf
@@ -21,6 +21,10 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
  
 #############################################
 # 2️⃣ EKS Node Group IAM Role
