02091c696098c9da2db2826e5ea72ce0e2925b95
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 10:40:35 +0530

    eks1

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -38,10 +38,10 @@ resource "aws_iam_role_policy_attachment" "alb_controller" {
 
 # Helm Provider Configuration
 provider "helm" {
-  kubernetes = {
+  kubernetes  {
     host                   = aws_eks_cluster.eks_cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    exec = {
+    exec  {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.eks_cluster.name, "--region", var.aws_region]
       command     = "aws"
