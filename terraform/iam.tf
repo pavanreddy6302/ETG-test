492b793231a4c00e5546f0d0d7b3b2166b26cdfa
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 21:26:22 +0530

    eks

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -253,17 +253,17 @@ resource "aws_iam_role_policy_attachment" "node_ssm_core" {
 # ##############################
 
 #This grants cluster-admin permissions to all users and roles
-resource "aws_eks_access_policy_association" "admin_policy_cluster_admin" {
-  cluster_name  = var.cluster_name
-  principal_arn = aws_iam_user.cluster_admin.arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+# resource "aws_eks_access_policy_association" "admin_policy_cluster_admin" {
+#   cluster_name  = var.cluster_name
+#   principal_arn = aws_iam_user.cluster_admin.arn
+#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   
-  access_scope {
-    type = "cluster"
-  }
+#   access_scope {
+#     type = "cluster"
+#   }
   
-  depends_on = [aws_eks_access_entry.cluster_admin_access]
-}
+#   depends_on = [aws_eks_access_entry.cluster_admin_access]
+# }
 
 # # EKS Access Policy for Rajat Kantjha
 # #resource "aws_eks_access_policy_association" "admin_policy_rajat" {
