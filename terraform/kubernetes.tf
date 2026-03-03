fb95b95bad7b47f29bcc3e6480a9f699e532e342
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 18:04:38 +0530

    eks

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -12,44 +12,44 @@ provider "kubernetes" {
   }
 }
 
-resource "kubernetes_cluster_role_binding" "admin_user" {
-  # Consolidate all dependencies into one depends_on block
-  depends_on = [
-    aws_eks_access_policy_association.admin_policy_cluster_admin,
-    aws_eks_access_entry.cluster_admin_access,
-    data.aws_eks_cluster.cluster1
-  ]
+# resource "kubernetes_cluster_role_binding" "admin_user" {
+#   # Consolidate all dependencies into one depends_on block
+#   depends_on = [
+#     aws_eks_access_policy_association.admin_policy_cluster_admin,
+#     aws_eks_access_entry.cluster_admin_access,
+#     data.aws_eks_cluster.cluster1
+#   ]
 
-  metadata {
-    name = "${var.cluster_name}-admin-binding"
-  }
+#   metadata {
+#     name = "${var.cluster_name}-admin-binding"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
 
-  subject {
-    kind      = "User"
-    name      = var.cluster_name
-    api_group = "rbac.authorization.k8s.io"
-  }
-}
-resource "kubernetes_cluster_role_binding" "alb_controller" {
-  metadata {
-    name = "alb-controller-binding"
-  }
+#   subject {
+#     kind      = "User"
+#     name      = var.cluster_name
+#     api_group = "rbac.authorization.k8s.io"
+#   }
+# }
+# resource "kubernetes_cluster_role_binding" "alb_controller" {
+#   metadata {
+#     name = "alb-controller-binding"
+#   }
 
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
-  }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "cluster-admin"
+#   }
 
-  subject {
-    kind      = "ServiceAccount"
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-  }
-}
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = "aws-load-balancer-controller"
+#     namespace = "kube-system"
+#   }
+# }
