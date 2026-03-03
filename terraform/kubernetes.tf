2950c7474855d68daed6562d66418631a7e2c684
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 18:06:59 +0530

    eks

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,16 +1,16 @@
-data "aws_eks_cluster" "cluster1" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster" "cluster1" {
+#   name = var.cluster_name
+# }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
-    command     = "aws"
-  }
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
+#     command     = "aws"
+#   }
+# }
 
 # resource "kubernetes_cluster_role_binding" "admin_user" {
 #   # Consolidate all dependencies into one depends_on block
