62a9a9e1ffd9d26e4b172d028cc0d5807396572e
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 17:18:17 +0530

    eks1

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,9 +1,9 @@
 # Helm Provider Configuration
 provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    exec {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
       command     = "aws"
@@ -62,30 +62,44 @@ resource "helm_release" "aws_load_balancer_controller" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
 
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
+set = [{name  = "clusterName"
+    type = "string"
+    value = var.cluster_name},
+    {name  = "serviceAccount.create"
+        value = "true"},
+    {name  = "serviceAccount.name"
+        value = "aws-load-balancer-controller"
+        type  = "string"},
+        {name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+        value = aws_iam_role.alb_controller.arn},
+    {name  = "serviceAccount.name"
+        value = "vpcId"
+        type  = aws_vpc.eks_vpc.id}]
 
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
+#   set {
+#     name  = "clusterName"
+#     value = var.cluster_name
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "true"
+#   }
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.alb_controller.arn
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
 
-  set {
-    name  = "vpcId"
-    value = aws_vpc.eks_vpc.id
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.alb_controller.arn
+#   }
+
+#   set {
+#     name  = "vpcId"
+#     value = aws_vpc.eks_vpc.id
+#   }
 
   timeout = 600
 
