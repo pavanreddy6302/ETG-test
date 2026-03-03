d266a1f1bcfe16b12d256dbd0305728ee5a086e1
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 20:46:17 +0530

    eks

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,55 +1,55 @@
-# data "aws_eks_cluster" "cluster1" {
-#   name = var.cluster_name
-# }
+data "aws_eks_cluster" "cluster1" {
+  name = var.cluster_name
+}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
-#     command     = "aws"
-#   }
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
+    command     = "aws"
+  }
+}
 
-# resource "kubernetes_cluster_role_binding" "admin_user" {
-#   # Consolidate all dependencies into one depends_on block
-#   depends_on = [
-#     aws_eks_access_policy_association.admin_policy_cluster_admin,
-#     aws_eks_access_entry.cluster_admin_access,
-#     data.aws_eks_cluster.cluster1
-#   ]
+resource "kubernetes_cluster_role_binding" "admin_user" {
+  # Consolidate all dependencies into one depends_on block
+  depends_on = [
+    aws_eks_access_policy_association.admin_policy_cluster_admin,
+    aws_eks_access_entry.cluster_admin_access,
+    data.aws_eks_cluster.cluster1
+  ]
 
-#   metadata {
-#     name = "${var.cluster_name}-admin-binding"
-#   }
+  metadata {
+    name = "${var.cluster_name}-admin-binding"
+  }
 
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
 
-#   subject {
-#     kind      = "User"
-#     name      = var.cluster_name
-#     api_group = "rbac.authorization.k8s.io"
-#   }
-# }
-# resource "kubernetes_cluster_role_binding" "alb_controller" {
-#   metadata {
-#     name = "alb-controller-binding"
-#   }
+  subject {
+    kind      = "User"
+    name      = var.cluster_name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+resource "kubernetes_cluster_role_binding" "alb_controller" {
+  metadata {
+    name = "alb-controller-binding"
+  }
 
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "ClusterRole"
-#     name      = "cluster-admin"
-#   }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
 
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = "aws-load-balancer-controller"
-#     namespace = "kube-system"
-#   }
-# }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+  }
+}
