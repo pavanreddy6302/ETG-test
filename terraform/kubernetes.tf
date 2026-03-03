ee4975c8e1e9ed270753978cd671ead8508deeff
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 15:44:59 +0530

    eks1

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,3 +1,11 @@
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+ 
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
 provider "kubernetes" {
   host                   = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
