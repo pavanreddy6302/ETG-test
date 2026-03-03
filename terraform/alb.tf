38cd67e526f7b9cea371c67a73982f3d8c28dd7a
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 17:25:23 +0530

    eks1

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -3,7 +3,7 @@ provider "helm" {
   kubernetes  {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    exec = {
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.aws_region]
       command     = "aws"
