b1195aa629d903aa686a6071f720a9f866fd0932
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-03 17:32:17 +0530

    eks1

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -62,44 +62,45 @@ resource "helm_release" "aws_load_balancer_controller" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
 
-  set  [{name  = "clusterName"
-    type = "string"
-    value = var.cluster_name},
-    {name  = "serviceAccount.create"
-        value = "true"},
-    {name  = "serviceAccount.name"
-        value = "aws-load-balancer-controller"
-        type  = "string"},
-        {name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-        value = aws_iam_role.alb_controller.arn},
-    {name  = "serviceAccount.name"
-        value = "vpcId"
-        type  = aws_vpc.eks_vpc.id}]
+#   set = [
+#     {name  = "clusterName"
+#     type = "string"
+#     value = var.cluster_name},
+#     {name  = "serviceAccount.create"
+#         value = "true"},
+#     {name  = "serviceAccount.name"
+#         value = "aws-load-balancer-controller"
+#         type  = "string"},
+#         {name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#         value = aws_iam_role.alb_controller.arn},
+#     {name  = "serviceAccount.name"
+#         value = "vpcId"
+#         type  = aws_vpc.eks_vpc.id}]
 
-#   set {
-#     name  = "clusterName"
-#     value = var.cluster_name
-#   }
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
 
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "true"
-#   }
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
 
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-load-balancer-controller"
-#   }
+  set  {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
 
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = aws_iam_role.alb_controller.arn
-#   }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.alb_controller.arn
+  }
 
-#   set {
-#     name  = "vpcId"
-#     value = aws_vpc.eks_vpc.id
-#   }
+  set {
+    name  = "vpcId"
+    value = aws_vpc.eks_vpc.id
+  }
 
   timeout = 600
 
