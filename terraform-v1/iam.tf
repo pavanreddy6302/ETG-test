b812805f8b684d8b8436c82c53da8419a4bfe676
pavanreddy6302 <pavankumarreddy6302@gmail.com>
2026-03-04 15:24:28 +0530

    eks

--- terraform-v1/iam.tf
+++ terraform-v1/iam.tf
@@ -105,15 +105,21 @@ resource "aws_iam_role" "github_actions_role" {
   
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"  # Update this with the appropriate principal for GitHub Actions
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         },
-        Action = "sts:AssumeRole"
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:pavanreddy6302/ETG-test:*"
+        }
       }
-    ]
+    }]
   })
 }
 
