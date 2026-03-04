6b6d0ec3fbd2f514046122a9d00b950015bcd353
pavan <pavankumarreddy6302.com>
2026-03-04 09:05:24 +0530

    eks

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-  region = var.region
-}
+# provider "aws" {
+#   region = var.region
+# }
 
 # Existing EKS cluster
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = var.cluster_name
+# }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = var.cluster_name
+# }
 
 # OIDC provider for cluster
 data "aws_iam_openid_connect_provider" "eks" {
