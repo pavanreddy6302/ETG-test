cdb43779db2d814a06ec6e44ece6b284337c9213
pavan <pavankumarreddy6302.com>
2026-03-04 09:24:59 +0530

    eks

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -55,18 +55,24 @@
 # }
 
 
+# Read the EKS cluster after it is created
 data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_name
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+
+  # Ensure Kubernetes provider waits for cluster creation
+  depends_on = [aws_eks_cluster.eks_cluster]
 }
 
 # Admin User ClusterRoleBinding
